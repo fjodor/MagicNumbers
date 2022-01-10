4f645d3e64661eb409ee53ce235b8b1422a8d537
Wolf Riepl <wolf.riepl@statistik-dresden.de>
2022-01-10 02:46:01 +0100

    Updated jobs. No parallelization - too much burden on machine even after job is finished!

--- R/_jobs.R
+++ R/_jobs.R
@@ -1,16 +1,16 @@
 # Jobs for magic numbers
 
 library(MagicNumbers)
-library(parallel)
+# library(parallel)
 # try(stopCluster(cl))
 # Doesn't exist when starting new local job
 # But recommendation: remove previous jobs before starting new one. There is an option to recover old jobs,
 # which may cost memory and stability.
 
-gc()
-cl <- makeCluster(parallelly::availableCores(omit = 1, logical = FALSE))
-library(future)
-plan(cluster)
+# gc()
+# cl <- makeCluster(parallelly::availableCores(omit = 1, logical = FALSE))
+# library(future)
+# plan(cluster)
 
 # This errors out: exceeds the maximum seed value!
 # seek_seeds_word(20e8:22e8, "programming", .eval = "future", cl)
@@ -27,21 +27,14 @@ plan(cluster)
 #---------------------------------------------------------#
 # Yet to calculate!
 
-# seek_seeds_word(0:3e8, "teacher", .eval = "future", cl)
-# seek_seeds_word(3e8:6e8, "teacher", .eval = "future", cl)
-# seek_seeds_word(6e8:9e8, "teacher", .eval = "future", cl)
-# seek_seeds_word(9e8:12e8, "teacher", .eval = "future", cl)
-seek_seeds_word(12e8:15e8, "teacher", .eval = "future", cl)
+seek_seeds_word(0:9e7, "figuren")
+seek_seeds_word(9e7:2e8, "figuren")
 
-seek_seeds_word(0:3e8, "animal", .eval = "future", cl)
-
-seek_seeds_word(0:3e8, "cruncher", .eval = "future", cl)
-
-seek_seeds_word(0:3e8, "rtistry", .eval = "future", cl)
-
-seek_seeds_word(0:3e8, "favorite", .eval = "future", cl)
-
-seek_seeds_word(0:3e8, "language", .eval = "future", cl)
-
-seek_seeds_word(0:3e8, "tidyverse", .eval = "future", cl)
+seek_seeds_word(12e8:15e8, "teacher")
+seek_seeds_word(0:3e8, "animal")
+seek_seeds_word(0:3e8, "cruncher")
+seek_seeds_word(0:3e8, "rtistry")
+seek_seeds_word(0:3e8, "favorite")
+seek_seeds_word(0:3e8, "language")
+seek_seeds_word(0:3e8, "tidyverse")
 
